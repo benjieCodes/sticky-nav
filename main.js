1844567a8db50361bb736117baf9c0f86c75ddd9
Benjie <benjiecodes@gmail.com>
2018-05-04 16:14:03 -0400

    added the class fixed-nav to body when top of browser hits bottom of navbar

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ const nav = document.querySelector("#main");
 const topOfNav = nav.offsetTop;
 
 function fixNav() {
-  console.log(topOfNav, window.scrollY);
+  if (window.scrollY >= topOfNav) {
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.classList.remove("fixed-nav");
+  }
 }
 
 window.addEventListener("scroll", fixNav);
