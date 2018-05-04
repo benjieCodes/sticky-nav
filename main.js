79685a2c31192c6bb46fede62257ba21fd66a67e
Benjie <benjiecodes@gmail.com>
2018-05-04 16:11:26 -0400

    added listener to grab the top location of nav bar

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+
+const nav = document.querySelector("#main");
+const topOfNav = nav.offsetTop;
+
+function fixNav() {
+    console.log(topOfNav);
+    
+  }
+}
+
+window.addEventListener("scroll", fixNav);
