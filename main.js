cc38c9ca0cf53748fcae8a3fcb0a21bb053f9e4d
Benjie <benjiecodes@gmail.com>
2018-05-04 16:12:09 -0400

    added window.scrollY to track top of the browser window

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ const nav = document.querySelector("#main");
 const topOfNav = nav.offsetTop;
 
 function fixNav() {
-    console.log(topOfNav);
-    
-  }
+  console.log(topOfNav, window.scrollY);
 }
 
 window.addEventListener("scroll", fixNav);
